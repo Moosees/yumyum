9d97ee0c265e819264cb40dbb32573f83a8dbabf
Moosees <linus.almgren@gmail.com>
2024-01-11 22:48:06 +0100

    clean up project, add routes

---
+++ src/views/EtaView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="eta">
+    <h1>ETA</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .eta {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/views/ReceiptView.vue
+++ src/views/ReceiptView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="receipt">
+    <h1>Receipt</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .receipt {
     min-height: 100vh;
     display: flex;
     align-items: center;
